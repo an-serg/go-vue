a10527612f7c51cbdf334e21939b1cd7a5897a8a
Nastya Sergeeva <sergeeva_a03@mail.ru>
2026-07-16 20:40:32 +0300

    Change enpoint for Swagger

--- auth/src/main.ts
+++ auth/src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
       .setVersion('1.0')
       .build();
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('/auth/api', app, document);
+    SwaggerModule.setup('/auth/docs', app, document);
 
     await app.listen(3000);
   }
