4b428cfda15095c4b6c1862cf17cc12ddedc6628
Nastya Sergeeva <sergeeva_a03@mail.ru>
2026-08-08 17:00:55 +0300

    Install certificate

--- auth/src/config/cookie.config.ts
+++ auth/src/config/cookie.config.ts
@@ -1,6 +1,6 @@
 export const cookie_config = {
     httpOnly: true,
-    secure: false, // Для локальной разработки HTTP
+    secure: process.env.COOKIE_SECURE,
     sameSite: 'strict' as const,
     accessMaxAge: 1 * 60 * 1000,
     refreshMaxAge: 3 * 60 * 1000,
