2b4ca3448d7efcc7b28e62d42381b5f63e525a21
root <root@8495513-ow854845.twc1.net>
2026-08-11 13:12:59 +0000

    Cookie config

--- auth/src/config/cookie.config.ts
+++ auth/src/config/cookie.config.ts
@@ -1,6 +1,6 @@
 export const cookie_config = {
     httpOnly: true,
-    secure: process.env.COOKIE_SECURE,
+    secure: process.env.COOKIE_SECURE === 'true',
     sameSite: 'strict' as const,
     accessMaxAge: 1 * 60 * 1000,
     refreshMaxAge: 3 * 60 * 1000,
